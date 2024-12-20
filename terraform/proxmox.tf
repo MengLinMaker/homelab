69b764707782612b6c6b89c8642e67191e0309a7
MengLinMaker <menglinmaker@gmail.com>
2024-12-21 03:10:06 +1100

    chore: simplify proxmox config

---
+++ terraform/proxmox.tf
@@ -0,0 +1,18 @@
+variable "proxmox_config" {
+  description = "Proxmox configuration"
+  type = object({
+    endpoint     = string
+    username  = string
+    password = string
+  })
+}
+
+provider "proxmox" {  
+  endpoint = var.proxmox_config.endpoint
+  username = var.proxmox_config.username
+  password = var.proxmox_config.password
+  insecure = true
+  ssh {
+    agent = true
+  }
+}
