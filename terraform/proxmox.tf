165aaf89333cebf99c60a17c918c696f3a928ef4
MengLinMaker <menglinmaker@gmail.com>
2024-12-21 11:08:50 +1100

    deploy other images

--- terraform/proxmox.tf
+++ terraform/proxmox.tf
@@ -1,10 +1,11 @@
 variable "proxmox_config" {
   description = "Proxmox configuration"
   type = object({
-    name     = string
-    endpoint = string
-    username = string
-    password = string
+    name         = string
+    endpoint     = string
+    username     = string
+    password     = string
+    datastore_id = string
   })
 }
 
